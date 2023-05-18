24252363b76094ffcef5309f5bd4dad1be33b53d
root <ms100891@cabrillopointacademy.org>
2023-05-18 01:03:36 +0000

    I worked on two folders

--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -9,7 +9,10 @@ if __name__ =='__main__':
     # 1. Ask the user if they know how to write code.
     adj=simpledialog.askstring(prompt='do you know how to wright code')
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
-    if adj=='yes'
+    if adj=='yes':
+        messagebox.showinfo('you will rule the world')
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    
+    if adj=='no':
+        messagebox.showinfo('error,sign up for classes at the league in python')
     # Run the window's .mainloop() method
+    window.mainloop()
--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,25 +1,26 @@
 from tkinter import messagebox, simpledialog, Tk
 
-# Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+    # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
+if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-    
+    window=Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
-
+    score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+    adj = simpledialog.askstring(prompt='do you know how to wright code')
     #      // 3.  Use an if statement to check if their answer is correct
-
+   if adj=='correct':
     #      // 4.  if the user's answer was correct, add one to their score 
- 
-    # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
+    if adj=='correct add one point to score':
+    # MAKE MORE QUESTIONS. Ask more questions by repeating the above
     #      // Option: Subtract a point from their score for a wrong answer
- 
+     if adj=='wrong subtract one point from score':
     # After all the questions have been asked, tell the user their final score
     # remember to convert your variable to a string using the str() function 
-    
+    if adj=='final score'str()
     # Run the window's .mainloop() method
+    window.mainloop()
